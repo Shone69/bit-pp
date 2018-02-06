c1bfc6738c1b0dad1dd8e1f9fc5dea28de0eb270
BIT Student <classroom@bgit.rs>
2018-02-06 16:31:01 +0100

    git-pp

---
+++ loops/index.js
@@ -0,0 +1,40 @@
+// for( var i = 0;i < 15; i++){
+//    if(i%2 === 0) {
+//        console.log("Even");
+//    }else{
+//        console.log("Odd");
+//    }
+// }
+
+// var t = 0;
+
+// for(var i = 0;i<1000;i++){
+//     if(i%3 == 0 && i%5 == 0){
+//     t +=i;
+//     }
+//     }
+// console.log(t);
+
+// var niz = [1, 2, 3, 4, 5, 6, 7, 8];
+// var zbir = 0;
+//  for(var i = 0; i < niz.length ; i++) {
+//      zbir += niz[i];
+
+//  }
+//  console.log(zbir); 
+
+// var x = ["1" , "A" , "B" , "c" , "r" , true , NaN,undefined];
+// for(var i = 0; i < x.length; i++){
+//     console.log(x[i]);
+// }
+
+// 
+
+// var t = 0;
+// for (var i = 0; i < 20; i++) {
+
+//     t += i * i;
+    
+// }
+// console.log(t);
+
---
+++ switch/switch2.js
@@ -0,0 +1,24 @@
+var a = 8;
+
+switch(a) {
+    case 1:
+        
+    case 2:
+       
+    case 3:
+        
+    case 4:
+        
+    case 5:
+        console.log("It is a weekday");
+        break;
+        
+    case 6:
+   
+    case 7:
+    
+    console.log("It is a weekend");
+        break;
+    default:console.log("Must  1-7");
+    
+}
